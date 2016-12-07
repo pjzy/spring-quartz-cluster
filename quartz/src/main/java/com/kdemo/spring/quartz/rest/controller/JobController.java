5b32e910a052831ff729e7eeade18504e0b69700
sayseakleng <sayseakleng@gmail.com>
2016-12-07 17:11:43 +0700

    Add UI to demo manually trigger, pause, and resume job

--- quartz/src/main/java/com/kdemo/spring/quartz/rest/controller/JobController.java
+++ quartz/src/main/java/com/kdemo/spring/quartz/rest/controller/JobController.java
@@ -1,5 +1,4 @@
 package com.kdemo.spring.quartz.rest.controller;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,19 +15,28 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kdemo.spring.quartz.dto.JobDTO;
 
+
 @RestController
 @RequestMapping("/api/jobs")
 public class JobController {
+	
 	private static final Logger logger = LoggerFactory.getLogger(JobController.class);
-
+	
 	@Autowired
 	private Scheduler scheduler;
 
+	
+	/**
+	 * Get list of registered jobs
+	 * @return
+	 * @throws SchedulerException
+	 */
 	@SuppressWarnings("unchecked")
 	@GetMapping
 	public List<JobDTO> getJobs() throws SchedulerException {
@@ -72,7 +80,14 @@ public class JobController {
 		return jobs;
 	}
 	
-	@GetMapping("/{jobGroup}/{jobName}")
+	
+	/**
+	 * Trigger a now now
+	 * @param jobGroup
+	 * @param jobName
+	 * @return
+	 */
+	@PostMapping("/{jobGroup}/{jobName}/trigger")
 	public Boolean fire(@PathVariable String jobGroup, @PathVariable String jobName) {
 		boolean isFire = false;
 				
@@ -85,5 +100,48 @@ public class JobController {
 		};
 		
 		return isFire;
+	}
+	
+	
+	/**
+	 * Pause a job
+	 * @param jobGroup
+	 * @param jobName
+	 * @return
+	 */
+	@PostMapping("/{jobGroup}/{jobName}/deactivate")
+	public Boolean pause(@PathVariable String jobGroup, @PathVariable String jobName) {
+		boolean isPause = false;
+				
+		JobKey jobKey = new JobKey(jobName, jobGroup);
+		try {
+			scheduler.pauseJob(jobKey);
+			isPause = true;
+		} catch (SchedulerException e) {
+			logger.error("");
+		};
+		
+		return isPause;
+	}
+	
+	/**
+	 * Resume a job
+	 * @param jobGroup
+	 * @param jobName
+	 * @return
+	 */
+	@PostMapping("/{jobGroup}/{jobName}/activate")
+	public Boolean resume(@PathVariable String jobGroup, @PathVariable String jobName) {
+		boolean isResume = false;
+				
+		JobKey jobKey = new JobKey(jobName, jobGroup);
+		try {
+			scheduler.resumeJob(jobKey);
+			isResume = true;
+		} catch (SchedulerException e) {
+			logger.error("");
+		};
+		
+		return isResume;
 	}
 }
